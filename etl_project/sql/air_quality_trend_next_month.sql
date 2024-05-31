09ee61e8d906b4a2253d149554258900ef90bd38
goosegofly <168407872+goosegofly@users.noreply.github.com>
2024-06-01 02:05:02 +0800

    Update air_quality_trend_next_month.sql
    update for more function used

--- etl_project/sql/air_quality_trend_next_month.sql
+++ etl_project/sql/air_quality_trend_next_month.sql
@@ -1,16 +1,21 @@
 select 
 	 air_quality_index
 	, province_id  
+	, province_name
 	, lag(air_quality_index, 1) over (partition by province_id order by year, month asc) as air_quality_prev_month
 	, month 
 	, year 
 from 
 	(select 
-		 province_id 
+		 t1.province_id 
+		 , province_name
 		, extract(month from date_time) as month 
 		, extract(year from date_time) as year 
 		, avg(air_quality_index) as air_quality_index 
 	from 
-		air_pollution 
-	group by 1,2,3
+		air_pollution t1
+	left join 
+		province t2 on t1.province_id = t2.province_id 
+	group by 1,2,3,4
 	) t1
+order by 6,5,2
